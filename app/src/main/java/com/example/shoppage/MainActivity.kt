932d03f343a4993e50b90d0b20612a08defc6abe
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-05-27 20:44:00 +0530

    added title bar with text

--- app/src/main/java/com/example/shoppage/MainActivity.kt
+++ app/src/main/java/com/example/shoppage/MainActivity.kt
@@ -3,44 +3,62 @@ package com.example.shoppage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.shoppage.ui.theme.CenturyOldStyleBold
 import com.example.shoppage.ui.theme.ShopPageTheme
+import com.example.shoppage.utils.annotations.VerticalScreenPreview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ShopPageTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TitleBar(
+                            onBackClick = {},
+                            onSearchClick = {},
+                            onFavouritesClick = {},
+                            onAddToCartClick = {}
+                        )
+                    }) { innerPadding ->
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun TitleBar(
+    onBackClick: () -> Unit,
+    onSearchClick: () -> Unit,
+    onFavouritesClick: () -> Unit,
+    onAddToCartClick: () -> Unit
+) {
+    TopAppBar(
+        title = { Text(text = "Shop", fontFamily = CenturyOldStyleBold) },
+        modifier = Modifier.fillMaxWidth()
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ShopPageTheme {
-        Greeting("Android")
-    }
+fun MainScreen(modifier: Modifier = Modifier) {
+
+}
+
+@VerticalScreenPreview
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
