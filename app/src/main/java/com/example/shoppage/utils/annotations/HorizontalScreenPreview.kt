9a757522d8aad0355d76c107c5ab419db69462cc
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-05-27 20:43:36 +0530

    created custom annotations for preview

---
+++ app/src/main/java/com/example/shoppage/utils/annotations/HorizontalScreenPreview.kt
@@ -0,0 +1,10 @@
+package com.example.shoppage.utils.annotations
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    showBackground = true,
+    widthDp = 640,
+    heightDp = 320
+)
+annotation class HorizontalScreenPreview
---
+++ app/src/main/java/com/example/shoppage/utils/annotations/VerticalScreenPreview.kt
@@ -0,0 +1,10 @@
+package com.example.shoppage.utils.annotations
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    showBackground = true,
+    widthDp = 360,
+    heightDp = 640
+)
+annotation class VerticalScreenPreview
